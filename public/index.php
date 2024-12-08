eb8d8adb2bad57563ab4a87f29e16a57bf30d872
Thrishanka051 <hmrthrishankaherath@gmail.com>
2024-12-08 13:22:43 +0530

    update index page

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+	<li><a href="createTrip.php"><strong>Create</strong></a> - add a trip</li>
+	<li><a href="read.php"><strong>Read</strong></a> - find a trip</li>
+	<li><a href="home.php"><strong>Trip</strong></a> - home page</li>
+	<li><a href="passengerHome.php"><strong>Passenger</strong></a> - home page</li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
